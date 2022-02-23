284625753dff64f545c3d89310ae4b0abf16e3f6
JCDC9919 <80248523+JCDC9919@users.noreply.github.com>
2022-02-23 18:05:46 +0000

    Add files via upload

---
+++ multiclipboard.py
@@ -0,0 +1,59 @@
+#from multiprocessing.sharedctypes import Value
+import sys
+import clipboard
+import json
+
+SAVED_DATA = "clipboard.json"
+
+def save_data(filepath, data):
+    with open(filepath, "w") as f:
+        json.dump(data, f)
+
+
+def load_data(filepath):
+    try:
+        with open(filepath, "r") as f:
+            data = json.load(f)
+            return data
+    except:
+        return {}
+
+if len(sys.argv) == 2:
+    command = sys.argv[1]
+    data = load_data(SAVED_DATA)
+
+    if command == "save":
+        key = input("Enter a key: ")
+        data[key] = clipboard.paste()
+        save_data(SAVED_DATA, data)
+        print("data saved")
+    elif command == "load":
+        key = input("Enter a key: ")
+        if key in data:
+            clipboard.copy(data[key])
+            print("data copied to clipboard")
+        else:
+            print("key does not exist")
+
+    elif command == "list":
+        print(data)
+
+    elif command == "delete":
+        key = input(str("Enter a key to delete: "))
+        if key in data:
+            del data[key]
+            print(data)
+            
+        else:
+            print("Key doesn\'t exist")
+          
+    
+
+    else:
+        print("unknown command")
+
+else:
+    print("please pass exactly one command")
+
+
+
